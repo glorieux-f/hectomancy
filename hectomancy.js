e432736430d6df78132f83b9f9d7765018f5a4c6
Frédéric Glorieux <frederic.glorieux@fictif.org>
2024-11-26 20:07:44 +0100

    plus de noms, manque encore d’ordre

---
+++ hectomancy.js
@@ -0,0 +1,155 @@
+const captions = [
+    // 00
+    
+    // ça, verbe, nom, nous
+    "",                 // 0
+    "il",               // 1
+    "elle",             // 2
+    "quand",            // 3
+    "où",               // 4
+    "je",                   // 5
+    "verbe",            // 6
+    "vouloir",          // 7
+    "nom",              // 8
+    "devoir",           // 9
+
+    // 10
+
+    "transparent",  // 0
+    "bleu",         // 1
+    "rouge",        // 2
+    "blanc",        // 3
+    "noir",         // 4
+    "brun",             // 5
+    "or",           // 6
+    "vert",         // 7
+    "gris",         // 8
+    "rose",         // 9
+
+
+    // 20
+
+    // carré
+    "", // 0
+    "gauche",       // 1
+    "droite",       // 2
+    "haut",         // 3
+    "bas",          // 4
+    "croix",            // 5
+    "droit",        // 6
+    "",             // 7
+    "onde",         // 8
+    "",             // 9
+    
+    // 30
+
+    // argent, droit, politique
+    "",             // 0
+    "cause",        // 1
+    "but",          // 2
+    "religion",     // 3
+    "",             // 4
+    "politique",    // 5
+    "médecine",     // 6
+    "art",          // 7
+    "technique",    // 8
+    "science",      // 9
+
+
+    // 40
+
+    // feuille, verre
+    "matière",  // 0
+    "eau",      // 1
+    "feu",      // 2
+    "air",      // 3
+    "terre",    // 4
+    "chair",        // 5
+    "métal",    // 6
+    "tissu",    // 7
+    "pierre",   // 8
+    "",         // 9
+
+    // 50
+
+    "vie",              // 0
+    "",                 // 1
+    "rencontre",        // 2
+    "chance",           // 3
+    "maison",           // 4
+    "famille",              // 5
+    "naissance",        // 6
+    "projets",          // 7
+    "travail",          // 8
+    "dette",            // 9
+
+    // 60
+
+    "corps",            // 0
+    "oreille",          // 1
+    "œil",              // 2
+    "tête",             // 3
+    "ventre",           // 4
+    "cœur",                 // 5
+    "pied",             // 6
+    "bouche",           // 7
+    "main",             // 8
+    "nez",              // 9
+
+    // 70
+
+    // jour, nuit, soleil, nuage, vent, froid, chaud
+    "",         // 0
+    "mer",      // 1
+    "feu",      // 2
+    "ciel",     // 3
+    "terre",    // 4
+    "45",           // 5
+    "route",    // 6
+    "vent",     // 7
+    "mont",     // 8
+    "pluie",    // 9
+    
+    // 80
+
+    // outil, mur, digue, route
+    "roue",     // 0
+    "81",       // 1
+    "82",       // 2
+    "83",       // 3
+    "84",       // 4
+    "moteur",       // 5
+    "sabre",    // 6
+    "aile",     // 7
+    "aiguille", // 8
+    "voile",    // 9
+
+    // 90
+
+
+    // manger, grandir, attaquer, défendre, jeune , vieux, lent, vif
+
+    // musique, rythme, harmonie, chant, danse
+    "",             // 0
+    "musique",      // 1
+    "spectacle",    // 2
+    "inspiration",  // 3
+    "",             // 4
+    "écrire",           // 5
+    "76",           // 6
+    "rêve",         // 7
+    "répétition",   // 8
+    "idée",         // 9
+
+    "",                 // 0
+    "peuple",           // 1
+    "mœurs",            // 2
+    "état",             // 3
+    "pays",             // 4
+    "société",              // 5
+    "guerre",           // 6
+    "mythe",            // 7
+    "58",               // 8
+    "59",               // 9
+
+];
