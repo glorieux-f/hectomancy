1650aa4c2d68af915c4b63079a5a56fdcd72acee
Frédéric Glorieux <frederic.glorieux@fictif.org>
2024-12-24 01:38:41 +0100

    Nommer

--- hectomancy.js
+++ hectomancy.js
@@ -1,29 +1,29 @@
 const deca = [
     ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    ["espace", "est", "ouest", "sud", "nord", "centre", "nord-est", "sud-est", "nord-ouest", "sud-ouest"],
-    // nez
-    ["", "oreille", "œil", "tête", "ventre", "cœur", "pied", "bouche", "main", "peau"],
-    // matières, feuilles, pierre
-    ["verre", "eau", "feu", "air", "terre", "chair", "bois", "métal", "pierre", "feuille"],
-    // couleurs
+    ["espace", "est", "ouest", "sud", "nord", "ici", "nord-est", "sud-est", "nord-ouest", "sud-ouest"],
+    ["néant", "eau", "feu", "ciel", "terre", "centre", "foudre", "lac", "montagne", "vent"],
     ["transparent", "bleu", "rouge", "blanc", "noir", "jaune", "brun", "rose", "gris", "vert" ],
-    // jour, nuit, soleil, nuage, vent, froid, chaud
-    ["pays", "lune", "soleil", "ciel", "plaine", "ville", "foudre", "lac", "mont", "vent"],
+    // nez
+    ["os", "oreille", "œil", "tête", "ventre", "cœur", "pied", "bouche", "main", "peau"],
+    // matières, feuilles, pierre
+    // ["verre", "eau", "feu", "air", "terre", "chair", "bois", "métal", "pierre", "feuille"],
     // outil, mur, digue, route, arme
-    ["roue", "arc", "casque", "", "chaudron", "moteur", "route", "arbre", "nœud", "voile"],
+    // ["roue", "arc", "", "", "", "moteur", "pot", "armure", "nœud", "voile"],
+    // jour, nuit, soleil, nuage, vent, froid, chaud
+    // ["pays", "lune", "soleil", "ciel", "plaine", "ville", "foudre", "lac", "mont", "vent"],
     // ça, verbe, nom, nous vouloir-devoir ?
-    ["ça", "je", "toi", "quand", "où", "on", "verbe", "", "nom", ""],
-    // haut, bas, gauche, droite, onde
-    ["point", "cercle", "ligne", "angle", "carré", "centre", "", "", "cube", ""],
+    ["ça", "je", "toi", "quand", "où", "nous", "verbe", "qualité", "nom", "manière"],
+    // onde
+    // ["point", "cercle", "ligne", "angle", "carré", "centre", "", "", "cube", ""],
     // argent, droit, politique
-    ["", "cause", "but", "religion", "", "politique", "médecine", "art", "technique", "science"],
+    ["culture", "cause", "but", "religion", "économie", "politique", "médecine", "art", "technique", "science"],
         // travail, rencontre
-    ["vie", "", "couple", "chance", "maison", "famille", "naissance", "projets", "deuil", "dettes"],
+    ["vie", "santé", "couple", "chance", "maison", "famille", "naissance", "projets", "deuil", "dettes"],
     // manger, grandir, attaquer, défendre, jeune , vieux, lent, vif
     // musique, rythme, harmonie, chant, danse
     // ["", "musique", "spectacle", "inspiration", "", "écrire", "", "rêve", "répétition", "idée"],
     // politique
-    ["", "peuple", "mœurs", "état", "pays", "société", "guerre", "mythe", "", ""]
+    ["", "peuple", "mœurs", "état", "pays", "société", "guerre", "mythe", "loi", ""]
 
 ];
 const captions = [
@@ -43,54 +43,54 @@ const captions = [
     // 10
 
     "",  // 0
-    "abysse",         // 1
-    "avant",        // 2
-    "conflit",        // 3
-    "armée",         // 4
+    "tomber",         // 1
+    "entreprendre",        // 2
+    "contester",        // 3
+    "commander",  // 14, sinogramme équivoque
     "",           // 5
-    "délivrance",         // 6
-    "confinement",         // 7
-    "jeunesse",        // 8
-    "dissipation",         // 9
+    "libérer",         // 6
+    "enfermer",         // 7
+    "ignorer",        // 18, porc sous l’herbe ? opposé à , chasser le porc, courage
+    "disséminer",         // 9
 
 
     // 20
 
     "", // 0
-    "après",       // 1
-    "rayon",       // 2
-    "coopération",         // 3
-    "occulter",          // 4
+    "succéder",       // 1
+    "rayonner",       // 2
+    "exalter",         // 3
+    "obscurcir",          // 4
     "",            // 5
-    "abondance",        // 6
-    "mue",             // 7
-    "beauté",         // 8
-    "clan",             // 9
+    "abonder",        // 26, famille à l’intérieur, pb d’éclipse
+    "muer",             // 7
+    "orner",         // 8
+    "~clan",    // 29, famille , clan
     
     // 30
 
     "",             // 0
-    "attente",        // 1
-    "fortune",          // 2
-    "créatif",     // 3
-    "paix",             // 4
-    "",    // 5
-    "maturité",     // 6
-    "percée",          // 7
-    "grande réserve",    // 8
-    "petit réserve",      // 9
+    "attendre",        // 1
+    "avoir",          // 2
+    "régner",     // 3
+    "honorer",             // 4
+    "vouloir",    // 5
+    "pouvoir",     // 6
+    "trancher",          // 7
+    "soumettre",    // 8
+    "séduire",      // 9
 
     // 40
 
     "",  // 0
-    "union",      // 1
-    "progrès",      // 2
-    "stagnation",      // 3
-    "réceptif",    // 4
+    "niveler",      // 1
+    "arriver",      // 2
+    "refuser",      // 3
+    "accepter",    // 4
     "",        // 5
     "prévoir",    // 6
-    "rassemblement",    // 7
-    "rayer",   // 8
+    "rassembler",    // 7
+    "dépouiller",   // 8
     "contempler",         // 9
 
     // 50
@@ -111,53 +111,53 @@ const captions = [
     "",            // 0
     "germer",          // 1
     "mordre",              // 2
-    "innocence",             // 3
-    "retour",           // 4
+    "~innocence",             // 63, sans folie
+    "revenir",           // 4
     "",                 // 5
-    "ébranlement",             // 6
-    "poursuivre",           // 7
-    "avaler",             // 8
-    "bénéfice",              // 9
+    "réveiller",             // 66, ébranlement
+    "suivre",           // 7
+    "nourrir",             // 68
+    "déborder",              // 9
 
     // 70
 
     // jour, nuit, soleil, nuage, vent, froid, chaud
     "",         // 0
-    "articuler",      // 1
-    "séparer",      // 2
-    "marcher",     // 3
-    "approche",    // 4
-    "",           // 5
-    "noce",    // 6
-    "oracle, conversation",     // 7
-    "perte",     // 8
-    "centre",    // 9
+    "rythmer",      // 71, ponctuer, 
+    "opposer",      // 72
+    "réaliser",     // 73, réaliser la parole, mettre en œuvre
+    "détailler",    // 74, regarer de petites choses
+    "parler",           // 75
+    "échouer",    // 76 retour de la petite sœur, mariage raté
+    "échanger, diluer",     // 7
+    "blesser",     // 78
+    "convaincre",    // 79, vérité intérieure
     
     // 80
 
     "",     // 0
-    "obstacle",       // 1
-    "vagabond",       // 2
-    "retraite",       // 3
-    "chaudron, humilier",       // 4
+    "entraver",       // 1
+    "voyager",       // 2
+    "échapper",       // 3
+    "combiner",       // 4
     "",       // 5
-    "petit",    // 6
-    "influence",     // 7
-    "stable", // 8
-    "infiltration",    // 9
+    "traverser",    // 86, petite expérience (aspect)
+    "accorder",     // 87, pot et hache
+    "finir", // 8
+    "progresser",    // 89, graduellement
 
     // 90
 
     "",                 // 0
-    "puits",           // 1
-    "chaudron",            // 2
-    "rencontre",             // 3
-    "ascension",             // 4
+    "puiser",           // 91, puits
+    "cuire",            // 92, chaudron
+    "rencontrer",             // 3
+    "grimper",             // 4
     "",              // 5
-    "persévérer",           // 6
-    "masse critique",            // 7
-    "poison",               // 8
-    "obéissant",               // 9
+    "persévérer",           // 96, cœur-lunes
+    "subir",            // 97, grande expérience (aspect)
+    "empoisonner",               // 98, sorcellerie
+    "obéir",               // 9
 
 
   
