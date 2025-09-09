ad611613406ee78ff9cd33acd3d0bcfc9e6a1a67
Frédéric Glorieux <frederic.glorieux@fictif.org>
2025-09-09 13:19:30 +0200

    Deckant

---
+++ cartomancie.js
@@ -0,0 +1,396 @@
+const deck48 = {
+  "♥": {
+    "key": "♥",
+    "note": "relation (substance, causalité, communauté) ressentir et relier, amour, famille, sentiments…"
+  },
+  "♦": {
+    "key": "♦",
+    "note": "quantité (universel, particulier, singulier) construire et sécuriser, pratique, argent, affaires, travail…"
+  },
+  "♣": {
+    "key": "♣",
+    "note": "modalité (possible, réel, nécessaire)  bouger et comuniquer, chance, opportunités, croissance…"
+  },
+  "♠": {
+    "key": "♠",
+    "note": "qualité (affirmation, négation, limitation) réfléchir et trancher, épreuves, transformation…"
+  },
+
+  "1": {
+    "key": "1",
+    "note": "Origine, condition, intuition pure, bonne volonté, sentiment initial, potentiel, risque…"
+  },
+  "1♥": {
+    "key": "1♥",
+    "name": "Moi",
+    "maxim": "Sans toi il n’y a pas d’autres, tu (1) es la condition de tes relations (♥).",
+    "note": ""
+  },
+  "1♦": {
+    "key": "1♦",
+    "name": "Unité",
+    "maxim": "La mesure (♦) ramène le divers à une unité (1).",
+    "note": ""
+  },
+  "1♣": {
+    "key": "1♣",
+    "name": "Projet",
+    "maxim": "Tu ne sais pas ce qui n’est pas possible (♣), laisse ta bonne volonté (1) vouloir.",
+    "note": ""
+  },
+  "1♠": {
+    "key": "1♠",
+    "name": "Décision",
+    "maxim": "Entre l’être (1) et les apparences, il faut choisir (♠) ; dit oui, dit non, mais surtout, nuance.",
+    "note": ""
+  },
+
+  "2": {
+    "key": "2",
+    "note": "Autre, phénomène / noumène, autonomie / hétéronomie, sujet / relation, lien, choix…"
+  },
+  "2♥": {
+    "key": "2♥",
+    "name": "Couple",
+    "maxim": "Lie (♥) toi sans te confondre, de l’autre (2), sois autant cause et effet que partie et tout.",
+    "note": ""
+  },
+  "2♦": {
+    "key": "2♦",
+    "name": "Contrat",
+    "maxim": "Tu ne seras pas plus (♦) en prenant plus à l’autre (2), chacun devient soi en pensant à tous.",
+    "note": ""
+  },
+  "2♣": {
+    "key": "2♣",
+    "name": "Coopération",
+    "maxim": "Seul, peu est possible (♣), trouve avec qui (2) réaliser ce qui devrait être.",
+    "note": ""
+  },
+  "2♠": {
+    "key": "2♠",
+    "name": "Dilemme",
+    "maxim": "Deux (2) alternatives ne s’excluent pas toujours, si des apparences les séparent, elles peuvent partager une essence.",
+    "note": ""
+  },
+
+  "3": {
+    "key": "3",
+    "note": "Tiers, médiation, schématisme de l’imagination, maxime, sensibilité commune"
+  },
+  "3♥": {
+    "key": "3♥",
+    "name": "Fête",
+    "maxim": "L’imagination est le tiers (3) qui raccorde raisons et sentiments entre les gens (♥)",
+    "note": ""
+  },
+  "3♦": {
+    "key": "3♦",
+    "name": "Marché",
+    "maxim": "La loi (3) doit permettre la coopération prospère des intérêts (♦)",
+    "note": ""
+  },
+  "3♣": {
+    "key": "3♣",
+    "name": "Progrès",
+    "maxim": "",
+    "note": ""
+  },
+  "3♠": {
+    "key": "3♠",
+    "name": "Blessure",
+    "maxim": "La vérité (3) e",
+    "note": ""
+  },
+
+  "4": {
+    "key": "4",
+    "note": "Terrain, ressources, moyens, base, cadre normatif, table des catégories"
+  },
+  "4♥": {
+    "key": "4♥",
+    "name": "Foyer",
+    "maxim": "",
+    "note": ""
+  },
+  "4♦": {
+    "key": "4♦",
+    "name": "Épargne",
+    "maxim": "",
+    "note": ""
+  },
+  "4♣": {
+    "key": "4♣",
+    "name": "Procédures",
+    "maxim": "",
+    "note": ""
+  },
+  "4♠": {
+    "key": "4♠",
+    "name": "Repos",
+    "maxim": "",
+    "note": ""
+  },
+
+  "5": {
+    "key": "5",
+    "note": "Tensions, frictions, pivot, conflit critique, antinomies de la raison, luttes des mobiles, sublime"
+  },
+  "5♥": {
+    "key": "5♥",
+    "name": "Jalousies",
+    "maxim": "",
+    "note": ""
+  },
+  "5♦": {
+    "key": "5♦",
+    "name": "Dépense",
+    "maxim": "",
+    "note": ""
+  },
+  "5♣": {
+    "key": "5♣",
+    "name": "Défi",
+    "maxim": "",
+    "note": ""
+  },
+  "5♠": {
+    "key": "5♠",
+    "name": "Soucis",
+    "maxim": "",
+    "note": ""
+  },
+
+  "6": {
+    "key": "6",
+    "note": "Lancement, nouveau, orientation, les idées régulatrices, l’impératif, comme si téléologique"
+  },
+  "6♥": {
+    "key": "6♥",
+    "name": "Réconciliation",
+    "maxim": "",
+    "note": ""
+  },
+  "6♦": {
+    "key": "6♦",
+    "name": "Don",
+    "maxim": "",
+    "note": ""
+  },
+  "6♣": {
+    "key": "6♣",
+    "name": "Aide",
+    "maxim": "",
+    "note": ""
+  },
+  "6♠": {
+    "key": "6♠",
+    "name": "Traversée",
+    "maxim": "",
+    "note": ""
+  },
+
+  "7": {
+    "key": "7",
+    "note": "Échanges, donner et accepter, "
+  },
+  "7♥": {
+    "key": "7♥",
+    "name": "Charme",
+    "maxim": "",
+    "note": ""
+  },
+  "7♦": {
+    "key": "7♦",
+    "name": "Spéculation",
+    "maxim": "",
+    "note": ""
+  },
+  "7♣": {
+    "key": "7♣",
+    "name": "Animation",
+    "maxim": "",
+    "note": ""
+  },
+  "7♠": {
+    "key": "7♠",
+    "name": "Ruse",
+    "maxim": "",
+    "note": ""
+  },
+
+  "8": {
+    "key": "8",
+    "note": "Cadre, seuil, discipline, discipline de la raison, ascétique de la liberté"
+  },
+  "8♥": {
+    "key": "8♥",
+    "name": "Limites",
+    "maxim": "",
+    "note": ""
+  },
+  "8♦": {
+    "key": "8♦",
+    "name": "Contrôle",
+    "maxim": "",
+    "note": ""
+  },
+  "8♣": {
+    "key": "8♣",
+    "name": "Organisation",
+    "maxim": "",
+    "note": ""
+  },
+  "8♠": {
+    "key": "8♠",
+    "name": "Contraintes",
+    "maxim": "",
+    "note": ""
+  },
+
+  "9": {
+    "key": "9",
+    "note": "Diffusion, influence, imprégnation, royaume des fins, finalité sans fin"
+  },
+  "9♥": {
+    "key": "9♥",
+    "name": "Rayonnement",
+    "maxim": "",
+    "note": ""
+  },
+  "9♦": {
+    "key": "9♦",
+    "name": "Commerce",
+    "maxim": "",
+    "note": ""
+  },
+  "9♣": {
+    "key": "9♣",
+    "name": "Pédagogie",
+    "maxim": "",
+    "note": ""
+  },
+  "9♠": {
+    "key": "9♠",
+    "name": "Chagrin",
+    "maxim": "",
+    "note": ""
+  },
+
+  "10": {
+    "key": "10",
+    "note": "Recommencement, idées de la raison, espérance"
+  },
+  "10♥": {
+    "key": "10♥",
+    "name": "Deuil",
+    "maxim": "",
+    "note": ""
+  },
+  "10♦": {
+    "key": "10♦",
+    "name": "Héritage",
+    "maxim": "",
+    "note": ""
+  },
+  "10♣": {
+    "key": "10♣",
+    "name": "Départ",
+    "maxim": "",
+    "note": ""
+  },
+  "10♠": {
+    "key": "10♠",
+    "name": "Mue",
+    "maxim": "",
+    "note": ""
+  },
+
+
+  "V": {
+    "key": "V",
+    "note": "jeunes, messagers, exécutants"
+  },
+  "V♥": {
+    "key": "V♥",
+    "name": "Allié",
+    "maxim": "",
+    "note": ""
+  },
+  "V♦": {
+    "key": "V♦",
+    "name": "Intermédiaire",
+    "maxim": "",
+    "note": ""
+  },
+  "V♣": {
+    "key": "V♣",
+    "name": "Camarade",
+    "maxim": "",
+    "note": ""
+  },
+  "V♠": {
+    "key": "V♠",
+    "name": "Étranger",
+    "maxim": "",
+    "note": ""
+  },
+
+  "D": {
+    "key": "D",
+    "note": "gestion, influence, cadrage"
+  },
+  "D♥": {
+    "key": "D♥",
+    "name": "Mère",
+    "maxim": "",
+    "note": ""
+  },
+  "D♦": {
+    "key": "D♦",
+    "name": "Gestionnaire",
+    "maxim": "",
+    "note": ""
+  },
+  "D♣": {
+    "key": "D♣",
+    "name": "Directrice",
+    "maxim": "",
+    "note": ""
+  },
+  "D♠": {
+    "key": "D♠",
+    "name": "Veuve",
+    "maxim": "",
+    "note": ""
+  },
+
+  "R": {
+    "key": "R",
+    "note": "Direction, stratégie, décision"
+  },
+  "R♥": {
+    "key": "R♥",
+    "name": "Père",
+    "maxim": "",
+    "note": ""
+  },
+  "R♦": {
+    "key": "R♦",
+    "name": "Notable",
+    "maxim": "",
+    "note": ""
+  },
+  "R♣": {
+    "key": "R♣",
+    "name": "Chef",
+    "maxim": "",
+    "note": ""
+  },
+  "R♠": {
+    "key": "R♠",
+    "name": "Juge",
+    "maxim": "",
+    "note": ""
+  }
+};
